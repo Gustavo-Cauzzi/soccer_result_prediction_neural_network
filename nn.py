cd63ccdf964c206f5e85de8dd8dca261373914a4
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2023-10-17 22:53:07 -0300

    feat: Epochs implementation

--- nn.py
+++ nn.py
@@ -1,12 +1,7 @@
 import csv
 import random
-import sys
 import math
-
-
-class Layer:
-    def __init__(neurons):
-        self.neurons = neurons
+import json
 
 class Neuron:
     def __init__(self):
@@ -20,15 +15,11 @@ class Neuron:
 data = []
 y = []
 result_row = 'FTR'
-unused_headers = ['FTHG', 'FTAG','HTR', 'HF', 'AF', 'HY', 'AY', 'HomeTeam', 'AwayTeam', 'HS', 'AS', 'ID'] # Talvez tirar os HALF TIME
-
-# Lembrar que é necessário adicionar o "ID" dentro do csv
-
-# X
+unused_headers = ['FTHG', 'FTAG', 'HTR', 'HF', 'AF', 'HY', 'AY', 'HomeTeam', 'AwayTeam', 'HS', 'AS', 'ID'] # Talvez tirar os HALF TIME
 
 with open("./Dataset_futebol.csv", 'r') as file:
     reader = csv.reader(file, delimiter=";")
-    headers = next(reader) # Read the header row
+    headers = next(reader)
 
     for row in reader:
         record = {}
@@ -42,7 +33,7 @@ with open("./Dataset_futebol.csv", 'r') as file:
             try:
                 record[key] = int(record[key])
             except:
-                a = 1
+                pass
         data.append(record)
     
     headers = list(filter(lambda h: not h in unused_headers and h != result_row, headers))
@@ -59,12 +50,44 @@ with open("./Dataset_futebol.csv", 'r') as file:
         for row in data:
             row[key] = (row[key] - lowest) / (biggest - lowest)
 
+test_data_ratio = 0.2 # 10% dos dados serão separados para teste
+def generate_test_data(x):
+    classes_occurrences = {}
+    for result in y:
+        if result in classes_occurrences:
+            classes_occurrences[result] += 1
+        else:
+            classes_occurrences[result] = 1
+    
+    for key, value in classes_occurrences.items():
+        classes_occurrences[key] = value * test_data_ratio
+
+    new_x = []
+    test_x = []
+
+    new_y = []
+    test_y = []
+
+    for idx, row in enumerate(x):
+        if classes_occurrences[y[idx]] > 1: # Ainda temos que pegar classes que são desse resultado
+            test_x.append(row)
+            test_y.append(y[idx])
+            classes_occurrences[y[idx]] -= 1
+        else:
+            new_x.append(row)
+            new_y.append(y[idx])
+
+    print(len(new_x), len(test_x))
+    return new_x, test_x, new_y, test_y
+
+data, test_data, y, test_y = generate_test_data(data)
+
 num_headers = len(headers)
 last_layer = 3
 layers_size = [num_headers, (num_headers + last_layer) // 2, last_layer]
 layers_indexes_ranges = []  
 
-learning_ratio = 0.2571649
+learning_ratio = 0.3
 
 s = 0
 for size in layers_size:
@@ -78,17 +101,11 @@ neurons = [
 
 neurons_qtd = sum(layers_size)
 weights_matrix = [
-    [random.random() for i in range(neurons_qtd)] for j in range(neurons_qtd)
+    [random.random() for _ in range(neurons_qtd)] for _ in range(neurons_qtd)
 ]
-
-# print(weights_matrix)
-# print(y)
-# print('neurons_qtd: ', layers_size)
 
 def sigmoidal(value):
     return 1 / (1 + math.exp(-value))
-
-# print(headers)
 
 result_map = {
     'H': [1, 0, 0],
@@ -96,7 +113,7 @@ result_map = {
     'D': [0, 0, 1],
 }
 
-for row_index, row in enumerate(data):
+def exec_neural_network_for_row(row):
     for header_index, header in enumerate(headers):
         neurons[header_index].value = row[header]
 
@@ -105,6 +122,7 @@ for row_index, row in enumerate(data):
             for neuron_index in range(layers_indexes_ranges[layer_index - 1], layers_indexes_ranges[layer_index]):
                 neuron = neurons[neuron_index]
                 neuron.value = sigmoidal(sum(neuron.buffer))
+                neuron.buffer = []
 
         if layer_index == len(layers_size) - 1:
             break
@@ -114,26 +132,104 @@ for row_index, row in enumerate(data):
             current_neuron_value = neurons[neuron_index].value
             for next_layer_neuron_index in range(layers_indexes_ranges[layer_index], layers_indexes_ranges[layer_index + 1]):
                 neurons[next_layer_neuron_index].buffer.append(current_neuron_value * weights_matrix[neuron_index][next_layer_neuron_index])
-        
-    expected_results = result_map[y[row_index]]
-    for idx, neuron_index in enumerate(range(layers_indexes_ranges[1], layers_indexes_ranges[2])):
-        neuron = neurons[neuron_index]
-        error_fator = (expected_results[idx] - neuron.value)
-        neuron.error = neuron.value * (1 - neuron.value) * error_fator
 
-    for neuron_index in range(layers_indexes_ranges[0], layers_indexes_ranges[1]):
-        neuron = neurons[neuron_index]
-        error_fator = 0
-        for last_layer_neuron_index in range(layers_indexes_ranges[1], layers_indexes_ranges[2]):
-            error_fator += neurons[last_layer_neuron_index].error * weights_matrix[neuron_index][last_layer_neuron_index]
-        neuron.error = neuron.value * (1 - neuron.value) * error_fator
+    results = []
+    for neuron_index in range(layers_indexes_ranges[1], layers_indexes_ranges[2]):
+        results.append(neurons[neuron_index].value)
+    return results
 
-    for layer_index in range(len(layers_size) - 1):
-        initial_index = 0 if layer_index == 0 else layers_indexes_ranges[layer_index - 1]
-        for neuron_index in range(initial_index, layers_indexes_ranges[layer_index]):
-            current_neuron_value = neurons[neuron_index].value
-            for next_layer_neuron_index in range(layers_indexes_ranges[layer_index], layers_indexes_ranges[layer_index + 1]):
-                next_layer_neuron_error = neurons[next_layer_neuron_index].error
-                weights_matrix[neuron_index][next_layer_neuron_index] = weights_matrix[neuron_index][next_layer_neuron_index] + learning_ratio * current_neuron_value * next_layer_neuron_error
-              
-print(weights_matrix)
+MAX_PATIANCE = 20
+last_epoch_error = 0
+current_patiance = 0
+best_wheights_matrix = weights_matrix
+while True: # Loop de épocas
+    current_epoch_error = 0
+
+    for row_index, row in enumerate(data):
+        exec_neural_network_for_row(row)
+
+        expected_results = result_map[y[row_index]]
+        for idx, neuron_index in enumerate(range(layers_indexes_ranges[1], layers_indexes_ranges[2])):
+            neuron = neurons[neuron_index]
+            error_fator = (expected_results[idx] - neuron.value)
+            neuron.error = neuron.value * (1 - neuron.value) * error_fator
+            # print(neuron.error, error_fator, neuron.value, expected_results[idx])
+            current_epoch_error += abs(neuron.error)
+
+        for neuron_index in range(layers_indexes_ranges[0], layers_indexes_ranges[1]):
+            neuron = neurons[neuron_index]
+            error_fator = 0
+            for last_layer_neuron_index in range(layers_indexes_ranges[1], layers_indexes_ranges[2]):
+                error_fator += neurons[last_layer_neuron_index].error * weights_matrix[neuron_index][last_layer_neuron_index]
+            neuron.error = neuron.value * (1 - neuron.value) * error_fator
+
+        for layer_index in range(len(layers_size) - 1):
+            initial_index = 0 if layer_index == 0 else layers_indexes_ranges[layer_index - 1]
+            for neuron_index in range(initial_index, layers_indexes_ranges[layer_index]):
+                current_neuron_value = neurons[neuron_index].value
+                for next_layer_neuron_index in range(layers_indexes_ranges[layer_index], layers_indexes_ranges[layer_index + 1]):
+                    next_layer_neuron_error = neurons[next_layer_neuron_index].error
+                    weights_matrix[neuron_index][next_layer_neuron_index] = \
+                        weights_matrix[neuron_index][next_layer_neuron_index] + learning_ratio * \
+                        current_neuron_value * next_layer_neuron_error
+    
+    if last_epoch_error < current_epoch_error:
+        current_patiance += 1
+        print(f"-------------------- ÉPOCA PIORRRRRR --------------------- {last_epoch_error} {current_epoch_error} ", current_patiance)
+    else:
+        best_wheights_matrix = [row.copy() for row in weights_matrix]
+        print(f"-=-=-=-=- ÉPOCA MELHOR -=-=-=-=-- {last_epoch_error} {current_epoch_error}" )
+        current_patiance = 0
+    
+    last_epoch_error = current_epoch_error
+    if current_patiance > MAX_PATIANCE:
+        break
+
+weights_matrix = best_wheights_matrix
+
+confusion_matrix = {}
+acertou = 0
+errou = 0
+for test_row_index, test_row in enumerate(test_data):
+    results = exec_neural_network_for_row(test_row)
+    max_neuron_index = results.index(max(results))
+
+    calculated_result = list(result_map.keys())[max_neuron_index]
+    expected_result = test_y[test_row_index]
+    if calculated_result == expected_result:
+        acertou += 1
+    else:
+        errou += 1
+
+    if expected_result not in confusion_matrix:
+        confusion_matrix[expected_result] = {}
+    if calculated_result not in confusion_matrix[expected_result]:
+        confusion_matrix[expected_result][calculated_result] = 0
+    confusion_matrix[expected_result][calculated_result] += 1
+
+    print("TEST ROW ----- ")
+    print(f'results: {results}')
+    print(f'max_neuron_index: {max_neuron_index}')
+    print(f'calculated_result: {calculated_result}')
+    print(f'test_y[test_row_index]: {test_y[test_row_index]}')
+
+print(f'Erro final considerado: {last_epoch_error}')
+
+print("Confusion Matrix:")
+for key, values in confusion_matrix.items():
+    print(f'{key} | {values}')
+
+for key, values in confusion_matrix.items():
+    print(f"Parâmetros do resultado:  {key}")
+    vps = confusion_matrix[key][key]
+    fns_vps = sum(confusion_matrix[key].values())
+    fps_vps = sum(map(lambda values: values[key], confusion_matrix.values()))
+
+    print(f"Recall: {vps / fns_vps} ({vps}, {fns_vps})")
+    print(f'Precision: {vps / fps_vps} ({vps}, {fps_vps})')
+
+print(f'acertou: {acertou}')
+print(f'errou: {errou}')
+print(f'Acurácia: {acertou * 100 / len(test_data)} {len(test_data)}')
+with open("Output.txt", "w") as text_file:
+    text_file.write(json.dumps(weights_matrix))
